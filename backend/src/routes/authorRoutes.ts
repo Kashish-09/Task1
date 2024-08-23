b53eb8d48af423a68a8b9b0694742069beef32f3
Kashish-09 <shahkashish953@gmail.com>
2024-08-23 22:42:03 +0530

    remove unnessesory

--- backend/src/routes/authorRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { createAuthors } from '../controllers/authorController';
-
-const router = Router();
-
-router.post('/authors/bulk', createAuthors);
-
-export default router;
--- backend/src/routes/bookRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { createBooks } from '../controllers/bookControllers';
-
-const router = Router();
-
-router.post('/books/bulk', createBooks);
-
-export default router;
